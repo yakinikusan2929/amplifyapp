c37a4d9062404351d6c4c7e6616fcec5fd6ffa03
yakinikusan2929 <gm.entry@gmail.com>
2023-12-26 14:10:18 +0900

    DB接続

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp9fa6572b": {
       "AppClientID": "string",
